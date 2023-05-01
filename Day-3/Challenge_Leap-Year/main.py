b10fe02fca17d8dd0658a5e089ba3ee7664d640d
G-H4wke <gabriel.hawke@outlook.com>
2023-05-02 00:30:20 +0100

    Added Challenge Leap year to check if a year is a Leap Year.

---
+++ Day-3/Challenge_Leap-Year/main.py
@@ -0,0 +1,15 @@
+# Don't change the code below
+year = int(input("Which year do you want to check? "))
+# Don't change the code above
+
+# Write your code below this line
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
