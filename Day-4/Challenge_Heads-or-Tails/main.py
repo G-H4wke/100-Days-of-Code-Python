cd65c3d5818c2a43eae656bb024677c574a28b79
G-H4wke <gabriel.hawke@outlook.com>
2023-05-03 20:48:19 +0100

    Added Challenge Heads or Tails to Day 4

---
+++ Day-4/Challenge_Heads-or-Tails/main.py
@@ -0,0 +1,10 @@
+# Write the rest of your code below this line 👇
+import random
+
+random_result = random.randint(0, 1)
+
+# Check Result
+if random_result == 0:
+    print("Tails")
+elif random_result == 1:
+    print("Heads")
