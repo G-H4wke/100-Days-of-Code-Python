b65946740d0fda836e52d213dda8b8629620f94d
G-H4wke <gabriel.hawke@outlook.com>
2023-05-04 19:07:53 +0100

    Added practice Dirty Dozen to Day 6

---
+++ Day-6/Practice_Dirty-Dozen/main.py
@@ -0,0 +1,11 @@
+# List of Fruits and Veg with the most pesticides!
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears",
+               "Tomatoes", "Celery", "Potatoes"]
+
+# Nested List with a mix of fruits and vegetables
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirt_dozen_combined = [fruits, vegetables]
+
+print(dirt_dozen_combined)
