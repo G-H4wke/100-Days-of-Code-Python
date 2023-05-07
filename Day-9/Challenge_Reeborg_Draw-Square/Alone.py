f16cf48f51459cf7f277d2f542728c41681d2313
G-H4wke <gabriel.hawke@outlook.com>
2023-05-07 10:16:28 +0100

    Added Day 9 and the Reeborg function code

---
+++ Day-9/Challenge_Reeborg_Draw-Square/Alone.py
@@ -0,0 +1,28 @@
+# https://www.reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json#
+# The code below was to practice functions with reeborg and will not run as a stand alone file.
+# You can use this code within the link posted above!
+from library import *
+
+# Make a square
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_around()
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
---
+++ Day-9/Practice_functions/main.py
@@ -0,0 +1,7 @@
+# Creating your own functions
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+my_function()
